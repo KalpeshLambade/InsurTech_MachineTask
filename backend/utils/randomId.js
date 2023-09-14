49a060ebd82d32e6ce9cbfb91dfa802f5d65e70e
Kalpesh Lambade <123794408+KalpeshLambade@users.noreply.github.com>
2023-09-14 13:21:25 +0530

    updated

--- backend/utils/randomId.js
+++ backend/utils/randomId.js
@@ -1,8 +1,18 @@
-export const randomgen = ()=> {
+export const randId = ()=> {
     const randomData = "0123456789";
+    const token = "";
 
     for(let i=0; i<10; i++){
-        
+        const randomIndex = Math.floor(Math.random()*randomData.length);
+
+        if(i==1){
+            token+="-";
+            continue;
+        }
+
+        token+= randomData.charAt(randomIndex);
     }
+
+    return token;
 
 }
