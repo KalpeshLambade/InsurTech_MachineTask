3f225d152b8c6788bd5c203df32d553ee02dc12b
Kalpesh Lambade <123794408+KalpeshLambade@users.noreply.github.com>
2023-09-15 15:46:59 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import Homepage from './components/Home/Homepage';
+import { NavbarDefault } from './components/Global/Navbar';
 
 function App() {
   return (
     <>
+      <NavbarDefault/>
       <Routes>
         <Route exact path='/' element={<Homepage/>}/>
       </Routes>
